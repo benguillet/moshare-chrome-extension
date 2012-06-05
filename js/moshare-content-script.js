47d3790077f79165e1ba4aca4a4c1c350370cb89
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-06-04 18:54:24 -0700

    Display moShare to Twitter properly!

--- js/moshare-content-script.js
+++ js/moshare-content-script.js
@@ -1,40 +1,32 @@
 (function($, window, undefined) {
-    var moshareIcon16URL = chrome.extension.getURL("icons/moshare-16.png");
+    var moshareIcon19URL = chrome.extension.getURL("icons/moshare-19.png");
 
     var moshareButtonTemplate = _.template(
-        '<a class="moshare-link" href="http://www.mogreet.com/moshare/it/?share=">' +
+        '<a class="moshare-link" href="http://www.mogreet.com/moshare/it/?share=<%= share %>&message=<%=  message %>&channel=chrome-plugin-twitter">' +
         '<img class="moshare-icon" alt="moShare icon" src="' + '<%= moshareIconURL %>' + '" />' +
         '</a>'
     );
 
-    var makeMoshareDiv = function() {
+    var makeMoshareDiv = function(tweetURL, tweetText) {
         return $(moshareButtonTemplate({
-            moshareIconURL: moshareIcon16URL
+            share: tweetURL,
+            message: tweetText,
+            moshareIconURL: moshareIcon19URL
         }));
     };
 
-    var extractScreenName = {
-        '.js-tweet-text':   function(idx, el) {
-            var old = $('a.tweet-screen-name', el);
-            if (old && old.length > 0) {
-                // "Old" Twitter selector.
-                //console.log(old);
-                return old;
-            } else {
-                // New-new Twitter "Fly" selector.
-                var fly = $('.fullname', el);
-                // Remap function of the node which has the text value we want (Screen-name.)
-                fly.text = function() { return $('.username b', el).text() };
-                //console.log(fly);
-                return fly;
-            }
+    var extractTextTweet = {
+        '.js-stream-tweet':   function(idx, el) {
+            var text      = $('p.js-tweet-text', el);
+            var permalink = $('a.js-permalink', el);
+            return [text, permalink];
         }
     };
 
     var foreachVisibleScreenName = function(fn) {
-        _.each(_.keys(extractScreenName), function(selector) {
+        _.each(_.keys(extractTextTweet), function(selector) {
             $(selector).each(function(idx, el) {
-                fn(extractScreenName[selector](idx, el), el);
+                fn(extractTextTweet[selector](idx, el), el);
             });
         });
     };
@@ -42,48 +34,19 @@
     var checkTweets = function() {
         foreachVisibleScreenName(function(el, parentEl) {
             if (0 === $('.moshare-link', parentEl).length) {
-                var moshareButton = makeMoshareDiv();     
+                var tweetText = encodeURIComponent(el[0].text());
+                var tweetURL  = encodeURIComponent('https://twitter.com/' + el[1].attr('href'));
+                var moshareButton = makeMoshareDiv(tweetURL, tweetText);     
                 moshareButton.click(function(e) {
-                    window.open('http://www.mogreet.com/moshare/it');
+                    window.open('http://www.mogreet.com/moshare/it/?share=' + tweetURL + '&message=' + tweetText + '&channel=chrome-plugin-twitter');
                     e.preventDefault();
                     e.stopPropagation();
                 });
-                $(el).append(moshareButton);
+                $(el[0]).append(moshareButton);
             }
         });
     };
 
-    
-    // var checkTweets = function() {
-    //     foreachVisibleScreenName(function(el, parentEl) {
-    //         var screenName = el.text();
-    //         var score = scores[screenName.toLowerCase()];
-    //         if (null != score && 0 === $('.klout-score-span', parentEl).length) {
-    //             var scoreDiv = makeScoreDiv(screenName, score, parentEl);
-    //             scoreDiv.click(function(e) {
-    //                 window.open('http://klout.com/user/' + screenName);
-    //                 e.preventDefault();
-    //                 e.stopPropagation();
-    //             });
-    //             $(el).before(scoreDiv);
-    //         }
-    //     });
-    // }
-
-    // var intervals = [ 500, 1000, 5000, 10000 ];
-    // var check = function() {
-    //     _.each(intervals, function(interval) {
-    //         _.delay(function() { checkTweets(); }, interval);
-    //     });
-    // };
-
-    // $(window).scroll(_.debounce(function() {
-    //     fetchMissingScores();
-    //     checkTweets();
-    // }, 500));
-
-    //$('body').live('mouseup', check);
-    //$(check);
     var intervals = [ 500, 1000, 5000, 10000 ];
     
     var check = function() {
